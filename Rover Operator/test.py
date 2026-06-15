bc5c0f852576393c2a7f1df3cbc8707e9a32a373
Sarthak Agarwal <sarth@Sarthaks-Macbook.local>
2026-06-15 12:09:48 +0100

    h

---
+++ Rover Operator/test.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt
+
+def on_message(client, userdata, msg):
+    print(f"{msg.topic}: {msg.payload.decode()}")
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected")
+    client.subscribe("senspr_read")  # or "#" for all topics
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883)
+client.loop_forever()
--- RoverSensors/src/radio.cpp
+++ RoverSensors/src/radio.cpp
@@ -1,5 +1,11 @@
 #include<Arduino.h>
 #include <radio.h>
 String getAge(){
+    byte b;
+    while(Serial.available()){
+        byte b = Serial.read();
+        Serial.print(char(b));
+
+    }
     return "";
 }
